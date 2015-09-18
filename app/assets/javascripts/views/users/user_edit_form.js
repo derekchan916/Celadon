48aeab3cc11e924f4e85e57f385fb747094ea254
App Academy Student <appacademy@Apps-Mac-mini-77.local>
2015-09-18 18:04:51 -0400

    Begin image file uploads.

--- app/assets/javascripts/views/users/user_edit_form.js
+++ app/assets/javascripts/views/users/user_edit_form.js
@@ -1,12 +1,13 @@
 Celadon.Views.UserEditForm = Backbone.View.extend({
   template: JST['users/edit_form'],
 
-  initialize: function(options) {
-    this.listenTo(this.model, 'sync', this.render)
+  events: {
+    'submit form': 'submit',
+    'change #input-post-image': 'fileInputChange'
   },
 
-  events: {
-    'submit form': 'submit'
+  initialize: function(options) {
+    this.listenTo(this.model, 'sync', this.render)
   },
 
   render: function() {
@@ -18,7 +19,7 @@ Celadon.Views.UserEditForm = Backbone.View.extend({
     e.preventDefault();
     var attrs = $(e.currentTarget).serializeJSON();
     var that = this;
-    
+
     this.model.set(attrs.user);
     this.model.save({}, {
       success: function() {
@@ -26,5 +27,44 @@ Celadon.Views.UserEditForm = Backbone.View.extend({
         Backbone.history.navigate('#/user/' + that.model.get('id'), { trigger: true });
       }
     })
+
+    // event.preventDefault();
+    //
+    // var title = this.$("#input-post-title").val();
+    // var file = this.$("#input-post-image")[0].files[0];
+    //
+    // var formData = new FormData();
+    // formData.append("post[title]", title);
+    // formData.append("post[image]", file);
+    //
+    // var that = this;
+    // this.model.saveFormData(formData, {
+    //   success: function(){
+    //     that.collection.add(that.model);
+    //     Backbone.history.navigate("", { trigger: true });
+    //   }
+    // });
+  },
+
+  fileInputChange: function(event){
+    console.log(event.currentTarget.files[0]);
+
+    var that = this;
+    var file = event.currentTarget.files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function(){
+      that._updatePreview(reader.result);
+    }
+
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      that._updatePreview("");
+    }
+  },
+
+  _updatePreview: function(src){
+    this.$el.find("#preview-post-image").attr("src", src);
   }
 })
