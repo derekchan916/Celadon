8691daa33d195c1a3fd7d39516b69ef1ddaa4c69
Celadon <derekchan916@gmail.com>
2015-09-20 18:13:23 -0400

    More CSS.

--- app/assets/javascripts/views/reviews/review_show.js
+++ app/assets/javascripts/views/reviews/review_show.js
@@ -1,6 +1,7 @@
 Celadon.Views.ReviewShow = Backbone.View.extend({
   tagName: 'ul',
   template: JST['reviews/show'],
+  tagName: 'review-list',
 
   render: function() {
     this.$el.html(this.template({
